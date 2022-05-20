af770acc53a19ec3db8cf5f38028cd40e66b6d89
Hudson8811 <hudson88@yandex.ru>
2022-05-20 11:05:13 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.points__container').ready(function(){
+	$('.main-anim').addClass('active');
+
+	$('.points__container').addClass('active');
+	$('.point').addClass('active');
+
+	$('.lines').addClass('active');
+
+	$('.main-body').addClass('active');
+});
